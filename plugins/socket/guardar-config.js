f95cfb1388ce34cc2b6b84285c49dc5f2f6440cf
𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔 <179016253+Fer280809@users.noreply.github.com>
2026-01-29 18:46:10 -0600

    Actualizar guardar-config.js

--- plugins/socket/guardar-config.js
+++ plugins/socket/guardar-config.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 
-// Cargar configuraciones al iniciar
+// Cargar configuraciones existentes
 try {
   const sessionsDir = `./${global.jadi}`
   if (fs.existsSync(sessionsDir)) {
@@ -19,7 +19,7 @@ try {
   }
 } catch (e) {}
 
-// Guardar automáticamente cada 3 minutos
+// Guardar automáticamente
 setInterval(() => {
   if (global.subBotConfigs) {
     Object.entries(global.subBotConfigs).forEach(([jid, config]) => {
@@ -34,21 +34,4 @@ setInterval(() => {
   }
 }, 180000)
 
-// Exportar funciones útiles
-export default {
-  saveConfig: (jid) => {
-    if (global.subBotConfigs?.[jid]) {
-      const folder = jid.split('@')[0]
-      const configPath = `./${global.jadi}/${folder}/config.json`
-      const dir = path.dirname(configPath)
-      if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true })
-      fs.writeFileSync(configPath, JSON.stringify(global.subBotConfigs[jid], null, 2))
-    }
-  },
-  
-  deleteConfig: (jid) => {
-    delete global.subBotConfigs?.[jid]
-    const configPath = `./${global.jadi}/${jid.split('@')[0]}/config.json`
-    if (fs.existsSync(configPath)) fs.unlinkSync(configPath)
-  }
-}
+export default {}
